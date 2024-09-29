ae8b1341f04cc97e6449290c3c138d24de35af29
RebinIsmail <149385501+RebinIsmail@users.noreply.github.com>
2024-09-29 20:21:44 +0200

    Car class og test

---
+++ TicketClassLibrary/Car.cs
@@ -0,0 +1,32 @@
+
+namespace TicketClassLibrary
+{
+    /// Repræsenterer en bil med et nummerplade og en dato.
+    public class Car
+    {
+
+        /// Får eller sætter nummerpladen for bilen.
+   
+        public string Licenseplate { get; set; }
+
+
+        /// Får eller sætter datoen knyttet til bilen.
+
+        public DateTime Date { get; set; }
+
+
+        /// Returnerer den faste pris for at krydse broen.
+        
+        public double Price()
+        {
+            return 240;
+        }
+
+        /// Returnerer typen af køretøjet.
+       
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
