4cc3ec7028b543de4e803dea7ce941031af9edc9
RebinIsmail <149385501+RebinIsmail@users.noreply.github.com>
2024-10-07 13:37:43 +0200

    test metoden opdateret

--- Testing/Licensplate.cs
+++ Testing/Licensplate.cs
@@ -1,33 +1,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TicketClassLibrary;
 
-namespace Testing 
+namespace Testing
 {
     [TestClass]
     public class VehicleTests
     {
         [TestMethod]
-        public void TestCarLicensePlate()
+        public void TestCarLicensePlate_ThrowsException()
         {
             // Arrange
             var car = new Car();
-            string longLicensePlate = "ABCDEFGHI"; // indeholder 9 characters
 
             // Act & Assert
-            var exception = Assert.ThrowsException<ArgumentException>(() => car.Licenseplate = longLicensePlate);
-            Assert.AreEqual("Nummerpladen må ikke overstige 7 tegn.", exception.Message);
+            Assert.ThrowsException<ArgumentException>(() => car.Licenseplate = "ABCD23232", "Nummerpladen skal være mindst 7 tegn");
         }
 
         [TestMethod]
-        public void TestMCLicensePlateLength_ThrowsException()
+        public void TestMCLicensePlate_ThrowsException()
         {
             // Arrange
             var mc = new MC();
-            string longLicensePlate = "12345678"; // indeholder 8 characters
 
             // Act & Assert
-            var exception = Assert.ThrowsException<ArgumentException>(() => mc.Licenseplate = longLicensePlate);
-            Assert.AreEqual("Nummerpladen må ikke overstige 7 tegn.", exception.Message);
+            Assert.ThrowsException<ArgumentException>(() => mc.Licenseplate = "12345678", "Nummerpladen mindst 7 tegn...tak!");
         }
     }
 }
