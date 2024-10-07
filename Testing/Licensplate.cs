7c26dd5f66f433ca586e04b3afd3647f1b4ac752
RebinIsmail <149385501+RebinIsmail@users.noreply.github.com>
2024-10-07 13:55:29 +0200

    f

--- Testing/Licensplate.cs
+++ Testing/Licensplate.cs
@@ -13,7 +13,7 @@ namespace Testing
             var car = new Car();
 
             // Act & Assert
-            Assert.ThrowsException<ArgumentException>(() => car.Licenseplate = "ABCD23232", "Nummerpladen skal være mindst 7 tegn");
+            Assert.ThrowsException<ArgumentException>(() => car.Licenseplate = "ABCD23232", "Nummerpladen skal være mindst 8 tegn");
         }
 
         [TestMethod]
@@ -23,7 +23,7 @@ namespace Testing
             var mc = new MC();
 
             // Act & Assert
-            Assert.ThrowsException<ArgumentException>(() => mc.Licenseplate = "12345678", "Nummerpladen mindst 7 tegn...tak!");
+            Assert.ThrowsException<ArgumentException>(() => mc.Licenseplate = "12345678", "Nummerpladen mindst 8 tegn...tak!");
         }
     }
 }
